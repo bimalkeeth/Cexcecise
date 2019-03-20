6535075a0d7082cd88a963d3f85939594b66023d
bimalkeeth@gmail.com <Scala@1234>
2019-03-20 23:06:35 +1100

    first

--- ex01/main.c
+++ ex01/main.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "main.h"
 
 int main() {
+    WhileLoop();
+    WhilePrintf();
+    ForLoop();
+    return 0;
+}
+ int WhileLoop(){
+     int i;
+     i=1;
+     while(i<=10){
+         printf("Hello world %d\n",i);
+         i++;
+     }
+     return 1;
+}
+int WhilePrintf(){
+    int i=1;
+    while(i<=printf("Hi\n")){
+        printf("Hello\n");
+        i++;
+    }
+    return 0;
+}
 
-    int aVar = 0;
+int ForLoop(){
+    int i,k;
+    long t;
+    t=time(NULL);
+    srand(t);
+    for(i=1;i<=10;i++){
 
-    scanf("%d",&aVar);
-    printf("The value %d\n",aVar);
+        k=rand()%10000;
+        printf("Hello world %d\n",k);
+
+    }
     return 0;
 }
---
+++ ex01/sample01/main.h
@@ -0,0 +1,11 @@
+//
+// Created by bill on 20/03/19.
+//
+
+#ifndef EX01_MAIN_H
+#define EX01_MAIN_H
+
+#endif //EX01_MAIN_H
+int WhileLoop();
+int WhilePrintf();
+int ForLoop();
